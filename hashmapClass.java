8ec75224718f80b78ee94e118712bf8b056830ea
Lisha1122 <160991814+Lisha1122@users.noreply.github.com>
2024-05-04 17:13:52 +0700

    Add files via upload

---
+++ hashmapClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lisha Khaerunniswah
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashmapClass {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("longName", "Lisha Khaerunniswah");
+        
+        System.out.println(map.get("longName"));
+    }
+}
