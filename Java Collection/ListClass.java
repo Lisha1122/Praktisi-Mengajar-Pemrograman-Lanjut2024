d1af2dcfd7d3f15a91239a5120add2d01fb5bc7d
Lisha1122 <160991814+Lisha1122@users.noreply.github.com>
2024-05-07 21:20:05 +0700

    Add files via upload

---
+++ Java Collection/ListClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lisha Khaerunniswah
+ */
+import java.util.List;
+import java.util.LinkedList;
+
+public class ListClass {
+    public static void main(String[] args) {
+        List<String> names = new LinkedList<>();
+        
+        names.add("Lisha");
+        
+        System.out.println(names);       
+    } 
+}
+
